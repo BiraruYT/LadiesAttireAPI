ab96c5c557c8eb6ab12a9a91ced5c42797da576a
Bilal <CheapPlayz@gmail.com>
2023-12-17 14:16:04 +0500

    Added Index Routes (not in use)

---
+++ routes/index.js
@@ -0,0 +1,12 @@
+var express = require('express');
+
+var router = express.Router();
+
+router.get('/', function(req, res) {
+    return res.status(200).json({
+        message: "Nothing exists here so get out before everything collapses!",
+        error: "NOTHING-EXISTS-HERE"
+    });
+});
+
+module.exports = router;
