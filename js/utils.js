6b3297c76ead4d26cf05612d04c2d14974384387
Bilal <CheapPlayz@gmail.com>
2023-12-17 14:20:15 +0500

    Changed Utils (not being used anymore)

--- js/utils.js
+++ js/utils.js
@@ -1,12 +1,1 @@
-function isInt(value) {
-    var x;
-    if (isNaN(value)) {
-        return false;
-    }
-    x = parseFloat(value);
-    return true
-}
-
-module.exports = {
-    isInt
-};
+module.exports = {};
