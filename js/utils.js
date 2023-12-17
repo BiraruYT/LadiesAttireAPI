4f8acdd5f161fc434e22b694845d8b0a524cee71
Bilal <CheapPlayz@gmail.com>
2023-12-17 16:28:01 +0500

    Added Utils Again (remembered that it had a purpose)

--- js/utils.js
+++ js/utils.js
@@ -1,1 +1,12 @@
-module.exports = {};
+function isInt(value) {
+    var x;
+    if (isNaN(value)) {
+        return false;
+    }
+    x = parseFloat(value);
+    return true
+}
+
+module.exports = {
+    isInt
+};
