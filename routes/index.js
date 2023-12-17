b3384c1e99030ae7ce3bcffcc03f60f9b3310354
Bilal <CheapPlayz@gmail.com>
2023-12-17 22:52:00 +0500

    Changed Some Status Codes

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res) {
-    return res.status(200).json({
+    return res.status(400).json({
         message: "Nothing exists here so get out before everything collapses!",
         error: "NOTHING-EXISTS-HERE",
     });
