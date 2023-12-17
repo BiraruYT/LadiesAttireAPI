f177dc012b1168bf3b12f71f1d6f1af2120bdcee
Bilal <CheapPlayz@gmail.com>
2023-12-17 22:59:08 +0500

    Changed Status Code

--- routes/services.js
+++ routes/services.js
@@ -3,7 +3,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/services', function(req, res) {
-    return res.status(200).json({
+    return res.status(400).json({
         message: "Nothing exists here so get out before everything collapses! But if your looking for some services, you can find them here:",
         services: [
             "services/user-to-id"
