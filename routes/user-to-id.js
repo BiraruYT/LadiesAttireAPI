af63ae2964659d92a3f4fcdfc6656cab39953f4d
Bilal <CheapPlayz@gmail.com>
2023-12-22 20:07:05 +0500

    Fixed Some Bugs In UserToID

--- routes/user-to-id.js
+++ routes/user-to-id.js
@@ -1,17 +1,13 @@
 const express = require('express');
 const SQLITE3 = require('better-sqlite3');
-const { JSDOM } = require('jsdom');
-const createDOMPurify = require("dompurify");
-const window = new JSDOM('').window;
-const DOMPurify = createDOMPurify(window);
+const { usersDB } = require("../app");
 
 const router = express.Router();
 
-const dbPath = "./sqlitedb/dev-users.db";
-const db = new SQLITE3(dbPath, { verbose: console.log });
+const db = new SQLITE3(usersDB);
 
 router.get('/user-to-id/:username', function(req, res) {
-    let username = req.params;
+    let username = req.params.username;
 
     if (typeof username !== 'string') {
         return res.status(400).json({
@@ -24,7 +20,7 @@ router.get('/user-to-id/:username', function(req, res) {
     const stmt = db.prepare('SELECT id FROM users WHERE username = ?');
     const user = stmt.get(username);
 
-    if (user.changes > 0) {
+    if (user) {
         res.status(200).json({
             message: 'User ID found.',
             userId: user.id,
